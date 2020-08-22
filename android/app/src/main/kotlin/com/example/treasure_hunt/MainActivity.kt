dc423e3a5402ccbcf54f33a9e1730c3cdeee95df
Brian Hong <bhong1992@gmail.com>
2020-08-21 21:52:36 -0700

    '08212020'

---
+++ android/app/src/main/kotlin/com/example/treasure_hunt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.treasure_hunt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+gitimport 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
