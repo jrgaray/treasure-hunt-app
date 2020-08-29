bea2179750273e5dfdc26fe89f82b6d42ee5659d
Brian Hong <bhong1992@gmail.com>
2020-08-29 10:28:11 -0700

    Changed folder structure, added simple form with validation

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import './screens/login.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Login(),
+    );
+  }
+}
