2df9d31a5a862d7c8453f4ce4ac9ff8f9021f2bd
camp <joseg707@gmail.com>
2020-09-23 10:44:59 -0700

    removed unused code in custom hook.

--- lib/hooks/useHuntAndChart.dart
+++ lib/hooks/useHuntAndChart.dart
@@ -1,10 +1,5 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 final treasureHunts = useState([]);
-// final treasureCharts = useState([]);
 void addTreasureHunt(newTreasureHunt) =>
     treasureHunts.value = [...treasureHunts.value, newTreasureHunt];
-
-// void addTreasureChart() {
-//   treasureCharts.value = [...treasureCharts.value, 'placeholder'];
-// }
