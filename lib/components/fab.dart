a4b4a2dc1aa804344d8dfc2f02c317ec6d743fbe
Jose Morris-Garay <joseg707@gmail.com>
2020-12-14 15:32:54 -0800

    Refetching dependencies

--- lib/components/fab.dart
+++ lib/components/fab.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:flutter/foundation.dart';
 part 'fab.g.dart';
 
 @swidget
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:treasure_hunt/app.dart';
-import 'package:provider/provider.dart';
 import 'package:treasure_hunt/state/treasure_chart_state.dart';
 import 'package:treasure_hunt/state/user_state.dart';
 
